30232f1cfe81edee2afa7ce83630277dc4e56d1b
benitaanawonah <benitastiles33@gmail.com>
2022-01-27 14:52:04 +0100

    added the search engine

--- src/app.js
+++ src/app.js
@@ -42,10 +42,27 @@ function displayTemperature(response) {
 }
 
 
+function search(city) {
+    let apiKey = "0f8c88146a435b8db9d6af1cacbbc02a";
+    
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-let apiKey = "0f8c88146a435b8db9d6af1cacbbc02a";
-let city = "Lagos"
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("Lisbon");
 
 
-axios.get(apiUrl).then(displayTemperature);
+
+
+
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
